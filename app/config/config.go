ad28869a96b9b53827aa2780c3bd658bdb947b97
Nattawit Chaiworawitsakul <nattawit.c@extreme.co.th>
2017-05-11 12:29:50 +0700

    Rewrite config package to use viper

--- app/config/config.go
+++ app/config/config.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	"encoding/xml"
 	"log"
 	"runtime"
 	"strings"
+
+	"github.com/spf13/viper"
 )
 
 const (
@@ -12,50 +13,25 @@ const (
 )
 
 var (
-	Default *Root
+	Config *Root
 )
 
 func init() {
 	_, currentPath, _, _ := runtime.Caller(1)
-	splitPath := strings.SplitAfter(currentPath, "exeoauth2/")
+	projectPath := strings.SplitAfter(currentPath, "exeoauth2/")[0]
 
-	var err error
-	// load default configurations
-	Default, err = Load(splitPath[0] + DefaultFile)
-	if err != nil {
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("main")
+	viper.AddConfigPath("conf/")
+
+	viper.AddConfigPath(projectPath + "conf/")
+
+	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalln(err)
 	}
-}
 
-type Root struct {
-	XMLName  xml.Name `xml:"itemcode-db"`
-	Server   *Server  `xml:"server"`
-	Database Database `xml:"database"`
-	LogPath  string   `xml:"log-path"`
-}
-
-type Server struct {
-	PublicListener  *Listener `xml:"public-listener"`
-	PrivateListener *Listener `xml:"private-listener"`
-}
-
-type Listener struct {
-	Address string `xml:"address"`
-	Tls     *Tls   `xml:"tls"`
-}
-
-type Tls struct {
-	Enable             bool   `xml:"enable,attr"`
-	CertificateFile    string `xml:"certificate-file"`
-	CertificateKeyFile string `xml:"certificate-key-file"`
-}
-
-type Database struct {
-	Redis Redis `xml:"redis"`
-}
-
-type Redis struct {
-	Address  string `xml:"address"`
-	Password string `xml:"password"`
-	DB       int64  `xml:"db"`
+	Config = &Root{}
+	if err := viper.Unmarshal(Config); err != nil {
+		log.Fatalln(err)
+	}
 }
---
+++ app/config/model.go
@@ -0,0 +1,42 @@
+package config
+
+type Root struct {
+	Server      Server      `mapstructure:"server"`
+	Database    Database    `mapstructure:"database"`
+	AccessToken AccessToken `mapstructure:"access-token"`
+	LogPath     LogPath     `mapstructure:"log-path"`
+}
+
+type Database struct {
+	Redis Redis `mapstructure:"redis"`
+	Tls   Tls   `mapstructure:"tls"`
+}
+
+type Tls struct {
+	Enable             bool   `mapstructure:"enable"`
+	CertificateFile    string `mapstructure:"certificate-file"`
+	CertificateKeyFile string `mapstructure:"certificate-key-file"`
+}
+
+type LogPath struct {
+	Error       string `mapstructure:"error"`
+	Request     string `mapstructure:"request"`
+	Transaction string `mapstructure:"transaction"`
+}
+
+type Redis struct {
+	Address       string `mapstructure:"address"`
+	Password      string `mapstructure:"password"`
+	AccessTokenDB int    `mapstructure:"access-token-db"`
+	ClientDB      int    `mapstructure:"client-db"`
+}
+
+type Server struct {
+	Address string `mapstructure:"address"`
+}
+
+type AccessToken struct {
+	TTL         uint `mapstructure:"time-to-live"`
+	TTLExpired  uint `mapstructure:"ttl-after-expire"`
+	TokenLength uint `mapstructure:"token-length"`
+}
